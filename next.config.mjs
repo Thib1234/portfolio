455672e9692d580268b2d7be22752114a7c6d9a0
thibault vandenbemden <44294626+Thib1234@users.noreply.github.com>
2024-09-02 18:40:29 +0200

    changing deploy file

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	experimental: {
-	  // outputStandalone: true, // Supprimer cette ligne
-	},
-	output: 'standalone', // Ajouter cette ligne
-	webpack: (config, { isServer }) => {
-	  if (!isServer) {
-		config.cache = {
-		  type: 'filesystem',
-		  buildDependencies: {
-			config: [__filename],
-		  },
-		};
-	  }
-	  return config;
-	},
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = {
+        type: 'filesystem',
+        buildDependencies: {
+          config: [__filename],
+        },
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
